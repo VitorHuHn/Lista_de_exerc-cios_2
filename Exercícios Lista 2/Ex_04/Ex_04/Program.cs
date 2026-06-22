cf49c992ee08977f10c594ae61053bb00c57835f
V1t0r_HuHn <104599280+VitorHuHn@users.noreply.github.com>
2026-06-22 01:35:23 -0300

    Add files via upload

---
+++ Exercícios Lista 2/Ex_04/Ex_04/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int a;
+            int b;
+            int area;
+
+            Console.Write("Informe a medida da altura do terreno: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Informe a medida da base do terreno: ");
+            b = int.Parse(Console.ReadLine());
+
+            area = a * b;
+
+            if (area > 100)
+            {
+                Console.WriteLine("Terreno Grande");
+            }
+            
+        }
+    }
+}
